b4249fdc04bd73c1900ff60c58afcc21ddfd3c4b
Raphael Schweizer <code@caringdev.ch>
2018-02-13 11:01:38 +0100

    complex model

---
+++ WebAPI/Controllers/ComplexController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+using SwaggerDemo.WebAPI.Models;
+
+namespace SwaggerDemo.WebAPI.Controllers
+{
+    [Route("api/complex")]
+    public class ComplexController : Controller
+    {
+        [HttpPost]
+        public void Create([FromBody] Complex model)
+        {
+        }
+
+        [HttpPut, Obsolete]
+        public void WrongCreate(Complex model)
+        {
+        }
+    }
+}
---
+++ WebAPI/Models/Complex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SwaggerDemo.WebAPI.Models
+{
+    public enum UserGroupPlatform
+    {
+        MeetUp,
+        Conference,
+        LinkedIn,
+        Xing,
+        Sharepoint
+    }
+
+    [Flags]
+    public enum Financing
+    {
+        None = 0,
+        Sponsored = 1,
+        SelfFunded = 2,
+        Paid = 4
+    }
+
+    /// <summary>
+    /// An arbitrarily complex type
+    /// </summary>
+    public class Complex
+    {
+        public DateTimeOffset Timestamp { get; set; }
+
+        /// <summary>
+        /// Can be null (obviously)
+        /// </summary>
+        public Complex Nested { get; set; }
+
+        public UserGroupPlatform Location { get; set; }
+
+        public Financing EntryFee { get; set; }
+    }
+}
